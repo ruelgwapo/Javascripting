038671cd19e450f257f1c3b7f8d3fc0ed7a273eb
Ruel P. Ybanez Jr <ruelybanez27@gmail.com>
2016-03-07 12:18:35 +0800

    javascripting

--- scope.js
+++ scope.js
@@ -5,13 +5,13 @@
 
          (function secondFunction(){
              var b = 8;
-
+console.log("a: "+a+", b: "+b+", c: "+c);
              (function thirdFunction(){
                  var a = 7, c = 9;
 
                  (function fourthFunction(){
                      var a = 1, c = 8;
-console.log("a: "+a+", b: "+b+", c: "+c);
+
 
                  })();
              })();
